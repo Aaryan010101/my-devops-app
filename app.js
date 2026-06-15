deb1075d09dbfa671a48077d98bdb0bb4e2c7839
Aaryan Thakur <taaryan918@gmail.com>
2026-06-15 17:39:28 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/hello', (req,res) => {
+    res.json({message: 'Hello from DevOps!', version: '1.0'});
+});
+
+app.listen(3000, () => {
+    console.log('App chal rahi hai port 3000 pe');
+});
